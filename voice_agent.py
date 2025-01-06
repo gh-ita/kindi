768778bbc922dd3141998de331f9b10178c699ef
gh-ita <hatimi.ghita@ensam-casa.ma>
2025-01-06 12:52:01 +0100

    didn t change anything

--- voice_agent.py
+++ voice_agent.py
@@ -1,23 +1,22 @@
-from smolagents import CodeAgent, LiteLLMModel, ManagedAgent, HfApiModel, TransformersModel
+from smolagents import CodeAgent, LiteLLMModel, ManagedAgent
 from huggingface_hub import login 
 from dotenv import load_dotenv
 import os
 from calendar_api import check_time_availability, add_task
 from task_setter_prompt import task_setter_system_prompt
-# Load model directly
-from transformers import AutoTokenizer, AutoModelForCausalLM
 
 #define tools
 #define the prompt 
-#use the transcriber 
+#use the transcriber twilio 
 
 load_dotenv()
 #HUGGINGFACEHUB_API_TOKEN
 huggingface_api_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+login(huggingface_api_token)
 #OPEN_AI_API_KEY
 openai_key = os.getenv('OPEN_AI_KEY')
 
-login(huggingface_api_token)
+
 model_id = 'openai/gpt-4o'
 model = LiteLLMModel( model_id = model_id, 
                      api_base = None, 
@@ -41,6 +40,8 @@ task_setting_agent = CodeAgent(tools=[check_time_availability, add_task],
 manager_agent = CodeAgent(tools=[], model=model, managed_agents=[managed_task_setting_agent])"""
 
 #manager_agent.run("Hello Kindi, I want you to add a washing dishes task starting from 10 am and ending at 11 am to my google calendar")
+
+
 task_setting_agent.run("Hello Kindi, I want you to add a washing dishes task starting from 10 am and ending at 11 am to my google calendar")
 
 
